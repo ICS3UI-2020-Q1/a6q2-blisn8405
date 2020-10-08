9709d4c3558c8bb0356795907d04a2d5a80465d8
blisn8405 <blisn8405@wrdsb.ca>
2020-10-08 13:49:20 +0000

    q2

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // crete scanner for user input
+    Scanner input= new Scanner(System.in);
+final int INTEGERS = 10;
+int number = 0;
+    //create an array with 10 spots
+    double[] user = new double[INTEGERS];
+//put integers into the array using a loop
+ System.out.println("Please enter in 10 integers to put into the array");
+for(int i =0; i < INTEGERS; i++){
+    user[i] = input.nextDouble();
+}
+
+// user a for loop to go through the array and find the number
+ System.out.println("Please enter a number to find");
+ number = input.nextInt();
+for(int i = 0; i < INTEGERS; i++){
+if(number == user[i]){
+System.out.println(user[i] +" is located at index " + (i+1));
+}
   }
+}
 }
